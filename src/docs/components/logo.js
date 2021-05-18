04b4a620dcf5326dba5621fd61e04f77c04a3546
Tugrul Erdem <tugrulerdemdogru@gmail.com>
2021-05-18 19:20:23 +0300

    added: logo

---
+++ src/docs/components/logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Path to the logo file on your project
+import rocketseatLogo from 'static/tedlogo.png';
+
+const Logo = () => (
+  <img src={rocketseatLogo} alt="Ted2xmen's logo" style={{ width: 180 }} />
+);
+
+export default Logo;
