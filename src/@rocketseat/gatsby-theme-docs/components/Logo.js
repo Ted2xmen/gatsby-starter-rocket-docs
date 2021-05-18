02a0b73f877da7845136041f6b1c332181a66457
Tugrul Erdem <tugrulerdemdogru@gmail.com>
2021-05-18 20:26:02 +0300

    added: logo

---
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Path to the logo file on your project
+import rocketseatLogo from 'assets/ted2xmen.png';
+
+const Logo = () => (
+  <img src={rocketseatLogo} alt="ted2xmen's logo" style={{ width: 180 }} />
+);
+
+export default Logo;
